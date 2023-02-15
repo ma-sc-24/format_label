2bc279451d523601431e555b3a8571395809d698
ma-sc-24 <msmasaca@gmail.com>
2023-02-15 15:32:58 -0600

    Format and documentation

--- lib/format_label.dart
+++ lib/format_label.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// Main class
+/// [size], [maxLines] and [textAlign] are optional properties
 class FormatLabel extends StatelessWidget {
   final String mainText;
   final String text;
@@ -9,32 +11,34 @@ class FormatLabel extends StatelessWidget {
   final int? maxLines;
   final TextAlign? textAlign;
 
-  const FormatLabel(
-      {Key? key,
-      required this.mainText,
-      required this.text,
-      required this.mainColor,
-      required this.descriptionColor,
-      this.size = 14, 
-      this.maxLines = 1, 
-      this.textAlign = TextAlign.left, 
-    })
-      : super(key: key);
+  /// Constructor
+  const FormatLabel({
+    Key? key,
+    required this.mainText,
+    required this.text,
+    required this.mainColor,
+    required this.descriptionColor,
+    this.size = 14,
+    this.maxLines = 1,
+    this.textAlign = TextAlign.left,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return RichText(
-      textAlign: textAlign!,
-      maxLines: maxLines,
+        textAlign: textAlign!,
+        maxLines: maxLines,
         text: TextSpan(
             style: TextStyle(
                 fontSize: size, fontWeight: FontWeight.bold, color: mainColor),
             text: '$mainText: ',
             children: [
-          TextSpan(
-              text: text,
-              style: TextStyle(
-                  fontSize: size, fontWeight: FontWeight.normal, color: descriptionColor))
-        ]));
+              TextSpan(
+                  text: text,
+                  style: TextStyle(
+                      fontSize: size,
+                      fontWeight: FontWeight.normal,
+                      color: descriptionColor))
+            ]));
   }
 }
